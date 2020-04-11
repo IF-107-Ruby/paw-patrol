5301a621f090282cd44d84c549282a541800d028
Vanya <vanyavasylyshyn@gmail.com>
2020-04-11 20:33:04 +0300

    Fix: removed null: false from units qr_link

--- db/migrate/20200411125438_create_units.rb
+++ db/migrate/20200411125438_create_units.rb
@@ -2,7 +2,7 @@ class CreateUnits < ActiveRecord::Migration[6.0]
   def change
     create_table :units do |t|
       t.string :name, null: false
-      t.string :qr_link, null: false
+      t.string :qr_link
 
       t.timestamps
     end
