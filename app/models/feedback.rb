117ad7929fdd68d8fe213fc810e6f68ece4a9a70
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-13 16:10:06 +0300

    Remove index and uniques validation for Feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -5,8 +5,7 @@ class Feedback < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, length: { maximum: 255 },
                     presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX }
   validates :describe, presence: true, length: { maximum: 255 }
 
   default_scope { order(created_at: :desc) }
