2e42f75260cf79bb6c9c6ede4927ed632233bba0
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-13 11:31:52 +0300

    Add validation in Feedback model

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :feedback do
-    user_full_name { "MyString" }
-    email { "MyString" }
-    describe { "MyText" }
+    user_full_name { Faker::Internet.username(specifier: 6..50) }
+    email { Faker::Internet.safe_email }
+    describe { Faker::Lorem.paragraph(sentence_count: 4) }
   end
 end
