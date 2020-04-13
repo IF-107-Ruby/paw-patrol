45ac1de8ec275c22a4f6c381ab32974a86d921de
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-13 23:17:13 +0300

    Refactoring before create PR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_411_125_438) do
+ActiveRecord::Schema.define(version: 20_200_413_130_249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
