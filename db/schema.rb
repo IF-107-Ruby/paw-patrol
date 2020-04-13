82ce28882fd7c0f52b7ac22fdb0ec0fffcab23bb
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-13 22:01:16 +0300

    Resolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_200_411_055_059) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
   create_table 'companies', force: :cascade do |t|
     t.string 'name', null: false
     t.text 'description'
