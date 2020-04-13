120c5fcf3e7f96b37eaf06cd9f5ff99dfb74c5ce
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-13 11:31:52 +0300

    Add validation in Feedback model

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,17 @@
 class Feedback < ApplicationRecord
+  before_save :downcase_email
+
+  validates :user_full_name, length: { in: 6..50 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, length: { maximum: 255 },
+                    presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :describe, presence: true, length: { maximum: 255 }
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Feedback, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'presence validation' do
+      it { is_expected.to validate_presence_of(:user_full_name) }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:describe) }
+    end
+
+    context 'length validation' do
+      it do
+        is_expected.to validate_length_of(:user_full_name)
+          .is_at_least(6)
+          .is_at_most(50)
+      end
+      it { is_expected.to validate_length_of(:email).is_at_most(255) }
+      it { is_expected.to validate_length_of(:describe).is_at_most(255) }
+    end
+
+    context 'uniqueness validation' do
+      subject { create(:feedback) }
+
+      it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    end
+
+    context 'Format validation' do
+      it { is_expected.to allow_value('test.email@gmail.com').for(:email) }
+      it { is_expected.not_to allow_value('not_email.com').for(:email) }
+    end
+  end
+
+  describe 'before_save downcase email' do
+    let(:feedback) { create(:feedback, email: 'TeSt.EmAil@gmaIl.Com') }
+
+    it 'email in lower case' do
+      expected_email = 'test.email@gmail.com'
+      expect(feedback.email).to eq(expected_email)
+    end
+  end
 end
