fed9a53435afdbd56e19455f40912f79b7a9a63f
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-14 16:11:35 +0300

    Add scope for order by created_at column and remove downcase_email method in Feedback model

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -23,13 +23,4 @@ RSpec.describe Feedback, type: :model do
       it { is_expected.not_to allow_value('not_email.com').for(:email) }
     end
   end
-
-  describe 'before_save downcase email' do
-    let(:feedback) { create(:feedback, email: 'TeSt.EmAil@gmaIl.Com') }
-
-    it 'email in lower case' do
-      expected_email = 'test.email@gmail.com'
-      expect(feedback.email).to eq(expected_email)
-    end
-  end
 end
