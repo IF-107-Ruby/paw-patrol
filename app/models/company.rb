ce0f8f8c0c3da5a4b7ae26c2b480458987cad50d
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-14 19:41:01 +0300

    Add optional email validation messages

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,10 +3,11 @@ class Company < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   VALID_PHONE_REGEX = /\A(\+)?([ 0-9]){10,14}\z/.freeze
   validates :email, presence: { message: 'can not be blank' },
-                    format: { with: VALID_EMAIL_REGEX, message: 'format is not valid' },
                     uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: 'format is not valid' },
+                    if: ->(c) { c.email.present? }
   validates :phone, format: { with: VALID_PHONE_REGEX,
                               message: 'is invalid: must be between 10 -
-                    15 digits long' },
+                    14 digits long' },
                     allow_blank: true
 end
