1f7df27d7fe7befac31075cff6b6605f3af1caaa
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-15 20:01:17 +0300

    Move constans up

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,7 @@
 class Feedback < ApplicationRecord
-  validates :user_full_name, length: { in: 6..50 }, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :user_full_name, length: { in: 6..50 }, presence: true
   validates :email, length: { maximum: 255 },
                     presence: true,
                     format: { with: VALID_EMAIL_REGEX }
