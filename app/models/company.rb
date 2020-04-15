5667fd0a94377dbff72072bb78811f67f5784c1c
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-15 10:38:16 +0300

    Add validation test

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,7 +7,6 @@ class Company < ApplicationRecord
   validates :email, format: { with: VALID_EMAIL_REGEX, message: 'format is not valid' },
                     if: ->(c) { c.email.present? }
   validates :phone, format: { with: VALID_PHONE_REGEX,
-                              message: 'is invalid: must be between 10 -
-                    14 digits long' },
+                              message: 'is invalid: must be from 10 to 14 digits long' },
                     allow_blank: true
 end
