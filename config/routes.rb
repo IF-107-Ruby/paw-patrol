10e3fc1d71036f108ea6c276c28aad465d7d231f
anton <anton.matviishyn@gmail.com>
2020-04-15 13:54:43 +0300

    Creating of preliminary environment for error pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   resources :units
   resources :feedbacks, only: %i[index show create destroy]
   resources :users
+  resources :companies
+
+  # Using :match so that error pages work for all types of requests, not just GET.
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#server_errors', via: :all
 end
