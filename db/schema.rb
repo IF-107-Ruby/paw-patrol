1fe67d2f25e770df7f43a7ebb23591e0fd49cb49
Vanya <vanyavasylyshyn@gmail.com>
2020-04-15 21:33:27 +0300

    fixed rubocop errors.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_413_072_154) do
+ActiveRecord::Schema.define(version: 20_200_413_070_244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
