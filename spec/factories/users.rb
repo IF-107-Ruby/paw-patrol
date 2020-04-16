5339b1771d18fd02d8854307edd453db5418519f
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-16 11:54:51 +0300

    Change user factories and add simple change to update test's in user controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user do
-    first_name { 'Test' }
-    last_name { 'User' }
-    is_admin { 1 }
-    sequence(:email) { |n| "user#{n}@example.com" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    is_admin { false }
+    email { Faker::Internet.safe_email }
+  end
+
+  factory :admin, parent: :user do
+    is_admin { true }
   end
 end
