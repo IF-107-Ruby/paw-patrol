87b1969bae0b8c31296c1a63adb9b49ee0bfcd72
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-16 11:21:15 +0300

    Change is_admin column type in Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_413_070_244) do
+ActiveRecord::Schema.define(version: 20_200_416_072_355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 20_200_413_070_244) do
     t.string 'first_name'
     t.string 'last_name'
     t.string 'email'
-    t.integer 'is_admin'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.boolean 'is_admin', default: false, null: false
   end
 end
