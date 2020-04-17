1babdb0d71ce6afc886568dfbcdea8995f8be6be
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-17 11:22:07 +0300

    Add model test

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  let(:unit) { create(:unit) }
+  let(:unit) { FactoryBot.create(:unit, :with_children, name: 'Parent unit') }
+
   describe 'Validation tests' do
     it 'is valid with valid attributes' do
       expect(unit).to be_valid
@@ -15,6 +16,27 @@ RSpec.describe Unit, type: :model do
     it 'is valid without qr_link' do
       unit.qr_link = nil
       expect(unit).to be_valid
+    end
+  end
+
+  describe 'Unit tree structure' do
+    let(:unit_child_1) { FactoryBot.create(:unit, :with_parent, name: 'Unit Child 1') }
+    let(:unit_child_2) { FactoryBot.create(:unit, :with_parent, name: 'Unit Child 2') }
+
+    it 'should have children' do
+      unit.children.create(name: 'Child 2')
+      expect(unit.ancestry).to be_nil
+      expect(unit.children).to_not be_empty
+      expect(unit.children.first.name).to eq('Child 1')
+      expect(unit.children.second.name).to eq('Child 2')
+    end
+
+    it 'should have parent' do
+      unit_child_1.parent = unit
+      unit_child_2.parent = unit
+      expect(unit_child_1.parent).to_not be_nil
+      expect(unit_child_1.ancestry).to eq(unit.id.to_s)
+      expect(unit_child_2.ancestry).to eq(unit.id.to_s)
     end
   end
 end
