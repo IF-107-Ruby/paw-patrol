d72e2a14888c2d039efcddbecba2a6b9bcc3d8eb
anton <anton.matviishyn@gmail.com>
2020-04-17 12:57:59 +0300

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20_200_416_082_345) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "companies", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.string "email", null: false
-    t.string "phone"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_companies_on_email", unique: true
+  create_table 'companies', force: :cascade do |t|
+    t.string 'name', null: false
+    t.text 'description'
+    t.string 'email', null: false
+    t.string 'phone'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_companies_on_email', unique: true
   end
 
   create_table 'feedbacks', force: :cascade do |t|
