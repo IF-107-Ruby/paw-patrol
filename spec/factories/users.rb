600f77eda31c96517a3b2318bffd71f13ff3f1ce
Vanya <vanyavasylyshyn@gmail.com>
2020-04-18 20:07:53 +0300

    modifyed user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,24 @@
 FactoryBot.define do
   factory :user do
-    first_name { 'Test' }
-    last_name { 'User' }
+    first_name { Faker::Internet.username(specifier: 5..10) }
+    last_name { Faker::Internet.username(specifier: 5..10) }
     is_admin { false }
-    sequence(:email) { |n| "user#{n}@example.com" }
+    email { Faker::Internet.safe_email }
+    sequence :password do |n|
+      "TestPassword#{n}"
+    end
+    sequence :password_confirmation do |n|
+      "TestPassword#{n}"
+    end
   end
 
   factory :admin, parent: :user do
     is_admin { true }
+  end
+
+  factory :super_user, parent: :user do
+    email { 'superuser@mail.com' }
+    password { 'super_user_password' }
+    password_confirmation { 'super_user_password' }
   end
 end
