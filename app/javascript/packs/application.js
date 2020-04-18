e199f88f55c1ee159919d0a012ff0ca56868dae0
Uliana Dzoba <55596322+ulianadzoba@users.noreply.github.com>
2020-04-18 18:06:59 +0300

    Fix/bootstap jquery (#71)
    * Make jQuery work
    
    * Make jQuery visible via $

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("popper.js")
+require("bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,7 +18,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery3
-//= require popper
-//= require bootstrap
-
