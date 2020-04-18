a33e1fa2fcd5a77fb1c68ebe88a244966d78197f
Vanya <vanyavasylyshyn@gmail.com>
2020-04-18 19:59:03 +0300

    aligned hash for rubocop.

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20_200_416_090_847) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.string 'email', default: '', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.boolean 'is_admin', default: false, null: false
@@ -51,8 +51,9 @@ ActiveRecord::Schema.define(version: 20_200_416_090_847) do
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token',
-                                      unique: true
+    t.index ['reset_password_token'],
+            name: 'index_users_on_reset_password_token',
+            unique: true
   end
 
   create_table 'users_companies_relationships', force: :cascade do |t|
