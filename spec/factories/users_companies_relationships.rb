21a6d4c198f5e3401d58da5f16f96b571f7f21e1
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-21 18:22:54 +0300

    Remove random role value from users_companies_relationship factory

--- spec/factories/users_companies_relationships.rb
+++ spec/factories/users_companies_relationships.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :users_companies_relationship do
     user { create(:user) }
     company { create(:company) }
-    role { (1..2).to_a.sample }
+    role { 1 }
   end
 
   factory :company_owner, parent: :users_companies_relationship do
