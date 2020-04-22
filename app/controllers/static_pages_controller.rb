58720fd53098a17aa9b8ad0ebc351898d40d611a
Uliana Dzoba <ulianadzoba@gmail.com>
2020-04-22 13:26:30 +0300

    Add StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def about; end
+
+  def services; end
+
+  def contact; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get  '/about', to: 'static_pages#about'
+  get  '/services', to: 'static_pages#services'
+  get  '/contact', to: 'static_pages#contact'
 
   resources :companies do
     resources :units
