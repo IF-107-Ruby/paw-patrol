1fa1e98f440b9fd442616cb64bc5878b370a595c
Vanya <vanyavasylyshyn@gmail.com>
2020-04-22 18:07:47 +0300

    created company registration form class.

---
+++ app/forms/company_registrations_form.rb
@@ -0,0 +1,48 @@
+class CompanyRegistrationsForm
+  include ActiveModel::Model
+
+  attr_accessor :name, :description, :company_email, :phone,
+                :first_name, :last_name, :user_email, :success
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  VALID_PHONE_REGEX = /\A(\+)?([ 0-9]){10,14}\z/.freeze
+
+  validates :name, presence: { message: 'can not be blank' }
+  validates :company_email, presence: { message: 'can not be blank' }
+  # uniqueness: { case_sensitive: false }
+  validates :company_email, format: { with: VALID_EMAIL_REGEX,
+                                      message: 'format is not valid' },
+                            if: ->(c) { c.company_email.present? }
+  validates :phone, format: { with: VALID_PHONE_REGEX,
+                              message: 'is invalid: must be from 10 to 14 digits long' },
+                    allow_blank: true
+
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :user_email, presence: true, length: { minimum: 8, maximum: 255 },
+                         format: { with: VALID_EMAIL_REGEX }
+
+  def save
+    ActiveRecord::Base.transaction do
+      if valid?
+        persist!
+        @success = true
+      else
+        @success = false
+      end
+    rescue StandardError => e
+      errors.add(:base, e.message)
+      @success = false
+    end
+  end
+
+  private
+
+  def persist!
+    company = Company.create!(name: name, description: description,
+                              email: company_email, phone: phone)
+    user = User.create!(first_name: first_name, last_name: last_name,
+                        email: user_email)
+    user.create_users_companies_relationship(user: user, company: company, role: 0)
+  end
+end
