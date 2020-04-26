eb8e8641cd3fedb0795643fb3464900a3df58ac6
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-26 18:12:43 +0300

    Set up action text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,5 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("trix")
+require("@rails/actiontext")
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_426_131_715) do
+ActiveRecord::Schema.define(version: 20_200_426_140_219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'action_text_rich_texts', force: :cascade do |t|
+    t.string 'name', null: false
+    t.text 'body'
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[record_type record_id name],
+            name: 'index_action_text_rich_texts_uniqueness',
+            unique: true
+  end
+
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id],
+            name: 'index_active_storage_attachments_uniqueness',
+            unique: true
+  end
+
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  end
 
   create_table 'companies', force: :cascade do |t|
     t.string 'name', null: false
@@ -78,6 +113,9 @@ ActiveRecord::Schema.define(version: 20_200_426_131_715) do
     t.index ['user_id'], name: 'index_users_companies_relationships_on_user_id'
   end
 
+  add_foreign_key 'active_storage_attachments',
+                  'active_storage_blobs',
+                  column: 'blob_id'
   add_foreign_key 'tickets', 'users'
   add_foreign_key 'units', 'companies'
   add_foreign_key 'users_companies_relationships', 'companies'
