5fb76b8def2f76fe6d439467c710f0570d0eddf0
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-26 22:14:35 +0300

    Add description column

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
 
-  validates :user, presence: true
+  has_rich_text :description
+
+  validates :user, :description, presence: true
   validates :name, presence: true, length: { in: 6..50 }
 end
--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :ticket do
-    name { 'MyString' }
-    user { nil }
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph }
+    user { create(:user) }
   end
 end
