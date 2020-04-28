cc1adb81b5b91120cd0df8463e34c2582ae5e48e
Vanya <vanyavasylyshyn@gmail.com>
2020-04-28 14:21:12 +0300

    changed @company to @current_company for members controller.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,15 +1,9 @@
 class MembersController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_company_by_id, only: :index
+  before_action :current_company, only: :index
 
   def index
-    @company_name = @company.name
-    @pagy, @members = pagy_decorated(@company.members, items: 10)
-  end
-
-  private
-
-  def find_company_by_id
-    @company = Company.find(params[:company_id])
+    @company_name = @current_company.name
+    @pagy, @members = pagy_decorated(@current_company.members, items: 10)
   end
 end
