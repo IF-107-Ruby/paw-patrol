f3836a8c67e3b8ff288c64887d2951031546bfd1
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-28 18:45:40 +0300

    Change feedback user_full_name validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,9 @@
 class Feedback < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
-  validates :user_full_name, length: { in: 6..50 }, presence: true
+  validates :user_full_name, presence: true
+  validates :user_full_name, length: { in: 6..50 },
+                             if: ->(f) { f.user_full_name.present? }
   validates :email, length: { maximum: 255 },
                     presence: true,
                     format: { with: VALID_EMAIL_REGEX }
