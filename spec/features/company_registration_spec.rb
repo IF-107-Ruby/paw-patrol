8c7930cb4099bfc4a3d80a7d03fc70935d352422
Vanya <vanyavasylyshyn@gmail.com>
2020-04-28 18:32:49 +0300

    modifyed integrational tests, removed registration form factory.

--- spec/features/company_registration_spec.rb
+++ spec/features/company_registration_spec.rb
@@ -3,27 +3,25 @@ require 'rails_helper'
 feature 'company registration' do
   let(:company_registration) { create :company_registrations_form_params }
 
-  scenario 'successfuly registrate company' do
+  scenario 'successfuly registrate company, log in and log out' do
     visit new_company_path
-    fill_in 'Company name', with: company_registration.name
-    fill_in 'Description', with: company_registration.description
-    fill_in 'Company email', with: company_registration.company_email
-    fill_in 'Phone', with: company_registration.phone
-    fill_in 'First Name', with: company_registration.first_name
-    fill_in 'Last Name', with: company_registration.last_name
-    fill_in 'User email', with: company_registration.user_email
-    fill_in 'company_registrations_form_password', with: company_registration.password
+    fill_in 'Company name', with: 'Name'
+    fill_in 'Description', with: 'Desc'
+    fill_in 'Company email', with: 'companyemail@company.com'
+    fill_in 'Phone', with: '+38091934561'
+    fill_in 'First Name', with: 'First Name'
+    fill_in 'Last Name', with: 'Last name'
+    fill_in 'User email', with: 'companyuseremail@company.com'
+    fill_in 'company_registrations_form_password', with: 'passwordtext'
     fill_in 'company_registrations_form_password_confirmation',
-            with: company_registration.password_confirmation
+            with: 'passwordtext'
     click_on 'Create Company'
 
     expect(page).to have_text('Company has been created.')
-  end
 
-  scenario 'successfuly log in and log out' do
     visit new_user_session_path
-    fill_in 'Email', with: company_registration.user_email
-    fill_in 'Password', with: company_registration.password
+    fill_in 'Email', with: 'companyuseremail@company.com'
+    fill_in 'Password', with: 'passwordtext'
     find('input', class: 'btn btn btn-primary').click
 
     expect(page).to have_text('Signed in successfully.')
