96aa96f17fb2ceeb3dd5c7583badf88b5d1a73c3
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-28 18:54:00 +0300

    Change feedback email validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,9 +4,10 @@ class Feedback < ApplicationRecord
   validates :user_full_name, presence: true
   validates :user_full_name, length: { in: 6..50 },
                              if: ->(f) { f.user_full_name.present? }
+  validates :email, presence: true
   validates :email, length: { maximum: 255 },
-                    presence: true,
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+                    if: ->(f) { f.email.present? }
   validates :message, presence: true, length: { maximum: 255 }
 
   scope :ordered_by_created_at, -> { order(created_at: :desc) }
