03f55a0e9c51ad930d3feed6ba0c3810581f7968
Vanya <vanyavasylyshyn@gmail.com>
2020-04-28 11:27:51 +0300

    moved new, create actions from members controller to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :obtain_user, only: %i[show edit update destroy]
+  before_action :current_company
 
   def index
     @pagy, @users = pagy_decorated(authorize(User.all), items: 10)
   end
 
   def show; end
+
+  def new
+    @user = authorize(@current_company.members.build)
+  end
+
+  def create
+    @user = @current_company.users.create(user_params)
+    @user.role = user_role_param[:role].to_i
+    if @user.save
+      flash[:success] = 'Company member created.'
+      redirect_to company_members_path(@current_company)
+    else
+      render 'new'
+    end
+  end
 
   def edit; end
 
@@ -36,5 +52,9 @@ class UsersController < ApplicationController
 
   def obtain_user
     @user = authorize(User.find(params[:id]).decorate)
+  end
+
+  def user_role_param
+    params.require(:user).permit(:role)
   end
 end
