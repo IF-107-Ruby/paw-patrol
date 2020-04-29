aeb2553f48588791a9aacaf9ef19a26dbbb45002
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-29 20:58:29 +0300

    Add ticket annotation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  user_id    :bigint           not null
+#  unit_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :unit
