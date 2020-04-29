151b717b4eeba1fcf7d71ad924709647c6cce109
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-04-29 11:50:29 +0300

    Ghange feedback user_full_name, and email validation

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,13 +1,10 @@
 class Feedback < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
-  validates :user_full_name, presence: true
-  validates :user_full_name, length: { in: 6..50 },
-                             if: ->(f) { f.user_full_name.present? }
-  validates :email, presence: true
+  validates :user_full_name, length: { in: 6..50 }, presence: true
   validates :email, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    if: ->(f) { f.email.present? }
+                    presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
   validates :message, presence: true, length: { maximum: 255 }
 
   scope :ordered_by_created_at, -> { order(created_at: :desc) }
