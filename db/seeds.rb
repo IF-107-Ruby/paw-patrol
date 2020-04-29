705b1a0044c0cb4c1dd4377c43e0536d0f917589
Vanya <vanyavasylyshyn@gmail.com>
2020-04-29 18:09:37 +0300

    modified seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
 require 'factory_bot_rails'
+if User.count == 0
+  FactoryBot.create(:admin,
+                    email: 'patrol.admin@gmail.com',
+                    password: '123456',
+                    password_confirmation: '123456')
 
-FactoryBot.create(:admin,
-                  email: 'patrol.admin@gmail.com',
-                  password: '123456',
-                  password_confirmation: '123456')
+  FactoryBot.create(:user,
+                    email: 'patrol.user@gmail.com',
+                    password: '123456',
+                    password_confirmation: '123456')
 
-FactoryBot.create(:user,
-                  email: 'patrol.user@gmail.com',
-                  password: '123456',
-                  password_confirmation: '123456')
+  10.times do
+    FactoryBot.create :user
+  end
+end
 
 10.times do
   FactoryBot.create(:company_with_units, units_count: 6)
@@ -16,10 +21,6 @@ end
 
 50.times do
   FactoryBot.create :feedback
-end
-
-10.times do
-  FactoryBot.create :user
 end
 
 Company.all.each do |company|
