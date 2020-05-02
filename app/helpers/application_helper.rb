1d9aab0ca5c07303784a763ba24fa9c61fcf057d
Liubomyr <lyubomur.user.mv@gmail.com>
2020-05-02 23:41:45 +0300

    Refactor views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def request_path_components
+    request.path.split('/')[1..-1]
+  end
+
+  def current_path(path)
+    'current' if current_page?(path)
+  end
+
+  def active_path(path)
+    'active' if current_page?(path)
+  end
 end
