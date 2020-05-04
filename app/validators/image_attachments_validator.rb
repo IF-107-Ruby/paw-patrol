78485d5e0e83b4bc98a4c6d771a85830aed94937
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-04 13:13:32 +0300

    Remove unused methods from ImageAttachmentsValidator

--- app/validators/image_attachments_validator.rb
+++ app/validators/image_attachments_validator.rb
@@ -3,17 +3,12 @@ class ImageAttachmentsValidator < ActiveModel::Validator
     return unless record.description&.body&.attachments&.any?
 
     record.description.body.attachments.each do |attach|
-      start_with_image_validation(attach, record)
       image_type_validation(attach, record)
       image_size_validation(attach, record)
     end
   end
 
   private
-
-  def start_with_image_validation(attach, record)
-    record.errors.add(:description, 'only images can be downloaded') unless attach.image?
-  end
 
   def image_type_validation(attach, record)
     return if attach.content_type.in?(%w[image/jpeg image/png image/gif])
