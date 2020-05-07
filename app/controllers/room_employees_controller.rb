cdc3550db9a443db99c64557eab2459f53c467c3
Vasyl <matsevko.vasil@gmail.com>
2020-05-07 22:54:00 +0300

    remove @unit_name variable

--- app/controllers/room_employees_controller.rb
+++ app/controllers/room_employees_controller.rb
@@ -3,7 +3,6 @@ class RoomEmployeesController < ApplicationController
   before_action :find_unit_by_id, only: :index
 
   def index
-    @unit_name = @unit.name
     @pagy, @room_employees = pagy_decorated(find_unit_by_id.users, items: 5)
   end
 
