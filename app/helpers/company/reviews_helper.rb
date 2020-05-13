6d42572713bfd96cdd67bc5b31b07b65c7781b2d
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-13 12:45:00 +0300

    Add review_action_link to ReviewsHelper module

---
+++ app/helpers/company/reviews_helper.rb
@@ -0,0 +1,11 @@
+class Company
+  module ReviewsHelper
+    def review_action_link(ticket)
+      link_to(new_company_ticket_review_path(ticket.id),
+              class: 'button ripple-effect margin-top-5 margin-bottom-10') do
+        content_tag(:i, ' ',
+                    class: 'icon-material-outline-thumb-up') + ' Leave a Review'
+      end
+    end
+  end
+end
---
+++ spec/controllers/company/reviews_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe Company::ReviewsController do
+  include_context 'employee with ticket'
+  let!(:staff_member) { create(:staff_member, company: company) }
+
+  describe 'GET #index' do
+    context 'User is employee' do
+      before { sign_in employee }
+
+      subject { get :index }
+
+      it { is_expected.to have_http_status(:success) }
+      it { is_expected.to render_template('index') }
+    end
+
+    context 'User is staff member' do
+      before { sign_in staff_member }
+
+      subject { get :index }
+
+      it { is_expected.to have_http_status(:redirect) }
+      it { is_expected.not_to render_template('index') }
+    end
+  end
+end
