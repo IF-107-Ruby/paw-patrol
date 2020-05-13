8d2bf1da4896713b9c0eeb6bd841996ac4210c56
Uliana Dzoba <ulianadzoba@gmail.com>
2020-05-13 10:47:06 +0300

    Fix errors

--- app/jobs/send_new_ticket_job.rb
+++ app/jobs/send_new_ticket_job.rb
@@ -3,6 +3,8 @@ class SendNewTicketJob < ApplicationJob
 
   def perform(ticket_id)
     ticket = Ticket.find_by(id: ticket_id)
+    return if ticket.nil?
+
     TicketMailer.new_ticket_email(ticket).deliver_now
   end
 end
--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -2,8 +2,6 @@ class TicketMailer < ApplicationMailer
   def new_ticket_email(ticket)
     @ticket = ticket
     @responsible_user = ticket.unit.responsible_user
-    @root_url = root_url
-    @url = company_ticket_url(@ticket)
 
     mail(to: @responsible_user.email, subject: 'New ticket')
   end
