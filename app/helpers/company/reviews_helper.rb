dbb11798e8dd73c377446d51b75a5e7eb22a4ef2
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-13 14:10:43 +0300

    Add ticket_reviewable_status method at ReviewsHelper

--- app/helpers/company/reviews_helper.rb
+++ app/helpers/company/reviews_helper.rb
@@ -1,5 +1,10 @@
 class Company
   module ReviewsHelper
+    def ticket_reviewable_status
+      content_tag(:span, 'Not Rated',
+                  class: 'company-not-rated margin-bottom-5')
+    end
+
     def review_action_link(ticket)
       link_to(new_company_ticket_review_path(ticket.id),
               class: 'button ripple-effect margin-top-5 margin-bottom-10') do
