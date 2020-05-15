46a2d42fac346f55591394cd9179fb3e7edcb8f0
Vanya <vanyavasylyshyn@gmail.com>
2020-05-15 18:58:22 +0300

    removed unnecessary tests, added feature ticket completion test.

--- spec/controllers/company/ticket_completions_controller_spec.rb
+++ spec/controllers/company/ticket_completions_controller_spec.rb
@@ -4,29 +4,9 @@ describe Company::TicketCompletionsController, type: :controller do
   include_context 'company with unit, ticket and ticket completion'
 
   describe 'Authentication and authorization tests' do
-    context 'if user can\'t create ticket completion' do
-      before do
-        sign_in unit.users.last
-      end
-
-      describe 'GET #new' do
-        subject { get :new, params: { ticket_id: unit.tickets.last.id } }
-
-        it { is_expected.to have_http_status(:redirect) }
-        it { is_expected.not_to render_template('new') }
-      end
-    end
-
     context 'if user can create ticket' do
       before do
         sign_in unit.responsible_user
-      end
-
-      describe 'GET #new' do
-        subject { get :new, params: { ticket_id: unit.tickets.last.id } }
-
-        it { is_expected.to have_http_status(:success) }
-        it { is_expected.to render_template('new') }
       end
 
       describe 'POST #create' do
@@ -56,7 +36,7 @@ describe Company::TicketCompletionsController, type: :controller do
 
           it { is_expected.to set_flash.now[:warning] }
           it { is_expected.not_to set_flash[:success] }
-          it { is_expected.to render_template('new') }
+          it { is_expected.to render_template('company/tickets/show') }
         end
       end
     end
--- spec/features/company/responsible_user_resolved_ticket.rb
+++ spec/features/company/responsible_user_resolved_ticket.rb
@@ -1,29 +1,33 @@
-# require 'rails_helper'
-#
-# feature 'Company owner set responsible user' do
-#   include_context 'company with unit, ticket and ticket completion'
-#   let!(:ticket_completion_attributes) { FactoryBot.attributes_for :ticket_completion }
-#
-#   before :each do
-#     login_as unit.responsible_user
-#     visit company_ticket_path(unit.tickets.last)
-#   end
-#
-#   scenario 'Successfuly resolve ticket' do
-#     find('i', class: 'complete-ticket-icon').click
-#
-#     print page.body
-#
-#     expect(page).to have_selector('.dashboard-headline h3', text: 'Add ticket completion')
-#     expect(page).to have_selector('.ticket-form')
-#
-#     fill_in_trix_editor('ticket_completion_description_trix_input_ticket_completion',
-#                         ticket_completion_attributes[:description])
-#
-#     click_on 'Resolve ticket'
-#
-#     expect(page).to have_selector('.notification.success.closeable',
-#                                   text: 'Ticket resolved!')
-#   end
-#
-# end
+require 'rails_helper'
+
+feature 'Responsible user resolve ticket' do
+  include_context 'company with unit, ticket and ticket completion'
+  let!(:ticket_completion_attributes) { FactoryBot.attributes_for :ticket_completion }
+
+  before :each do
+    login_as unit.responsible_user
+    visit company_ticket_path(unit.tickets.last)
+  end
+
+  scenario 'Successfuly resolve ticket' do
+    fill_in_trix_editor('ticket_completion_description_trix_input_ticket_completion',
+                        ticket_completion_attributes[:description])
+
+    click_on 'Resolve ticket'
+
+    expect(page).to have_selector('.notification.success.closeable',
+                                  text: 'Ticket resolved!')
+
+    expect(page).to have_selector('.headline', text: 'Ticket Completion')
+  end
+
+  scenario 'unsuccessfuly resolve ticket' do
+    fill_in_trix_editor('ticket_completion_description_trix_input_ticket_completion', '')
+
+    click_on 'Resolve ticket'
+
+    expect(page).to have_selector('.notification.warning.closeable',
+                                  text: 'Ticket is not resolved!')
+    expect(page).to have_text('Description can\'t be blank')
+  end
+end
