126337cd8e0d8ed86127f34d8ad8b85d02d1f907
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-18 23:02:01 +0300

    Write integration test

---
+++ spec/features/company/employee_leave_a_review_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'EmployeeLeaveAReview' do
+  include_context 'employee with ticket'
+  let(:review_attributes) { FactoryBot.attributes_for :review }
+
+  before do
+    login_as employee
+    visit company_reviews_path
+  end
+
+  scenario 'successfully' do
+    expect(page).to have_selector('h3', text: 'Reviews')
+    click_on 'Leave a Review'
+
+    expect(page).to have_selector('h3', text: 'Leave a Review')
+    choose('review_rating_3', option: '3')
+    fill_in 'review_comment', with: review_attributes[:comment]
+    within('.dashboard-content-inner') do
+      click_on 'Leave a Review'
+    end
+    expect(page).to have_selector('.notification.success.closeable',
+                                  text: 'Review saved!')
+  end
+
+  scenario 'unsuccessfully' do
+    click_on 'Leave a Review'
+
+    within('.dashboard-content-inner') do
+      click_on 'Leave a Review'
+    end
+
+    expect(page).not_to have_selector('.notification.success.closeable',
+                                      text: 'Review saved!')
+  end
+end
