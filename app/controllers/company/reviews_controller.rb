8a8e53fcad351496b7de90b870c59ab72e0b0521
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-18 21:19:51 +0300

    Implement edit review page

--- app/controllers/company/reviews_controller.rb
+++ app/controllers/company/reviews_controller.rb
@@ -1,5 +1,7 @@
 class Company
   class ReviewsController < Company::BaseController
+    before_action :find_review_by_id, only: %i[edit update]
+
     def index
       authorize([:company, Review])
       @pagy, @tickets = pagy_decorated(current_user.tickets, items: 10)
@@ -24,7 +26,22 @@ class Company
       end
     end
 
+    def update
+      if @review.update(review_params)
+        flash[:success] = 'Review updated!'
+        redirect_to company_reviews_path
+      else
+        flash.now[:warning] = 'Review is not updated!'
+        render :edit
+      end
+    end
+
     private
+
+    def find_review_by_id
+      @review = Review.find(params[:id])
+      authorize([:company, @review])
+    end
 
     def review_params
       params.require(:review).permit(:rating, :comment)
