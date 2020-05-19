64a6b05154b9ee660259d5bbffa8cd69b29a7678
Uliana Dzoba <ulianadzoba@gmail.com>
2020-05-19 15:26:35 +0300

    rebase

--- app/javascript/packs/watchers.js
+++ app/javascript/packs/watchers.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
     $("#add-watchers-btn").hide();
     $("#add-watcher-form").toggle("slide");
   });
+  $("body").on("click", "#cancel-watchers-btn", function () {
+    $("#add-watchers-btn").show();
+    $("#add-watcher-form").toggle("slide");
+  });
 });
