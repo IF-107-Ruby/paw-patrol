aa3d862adbb327206615bf8bc104126ebab2ba9e
Vanya <vanyavasylyshyn@gmail.com>
2020-05-20 14:30:01 +0300

    added tests for ticket decorator.

--- spec/decorators/ticket_decorator_spec.rb
+++ spec/decorators/ticket_decorator_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TicketDecorator do
   let!(:company) { create(:company) }
+  let!(:staff_member) { create(:staff_member, company: company, unit: unit ) }
   let!(:unit) { create(:unit, :with_employee_and_ticket, company: company) }
+  let!(:ticket_completion ) { create(user: staff_member, ticket: ticket) }
   let!(:ticket) { unit.tickets.first.decorate }
   let!(:user) { ticket.user.decorate }
 
@@ -12,5 +14,17 @@ RSpec.describe TicketDecorator do
 
   it 'created_by' do
     expect(ticket.created_by).to eq("Created by #{user.full_name}")
+  end
+
+  it 'display_status' do
+    expect(ticket.display_status).to eq('open')
+  end
+
+  it 'completion_description' do
+    expect(ticket.completion_description).to eq("#{ticket_completion.description}")
+  end
+
+  it 'completion' do
+    expect(ticket.completion).to eq(ticket_completion)
   end
 end
