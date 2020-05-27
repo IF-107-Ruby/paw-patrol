4d378e72320c5c177e1a2c2c1f821e87411196a0
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-27 20:55:25 +0300

    Simple changes at TicketsHelper

--- app/helpers/company/tickets_helper.rb
+++ app/helpers/company/tickets_helper.rb
@@ -2,9 +2,8 @@ class Company
   module TicketsHelper
     def ticket_reviewable_status(ticket)
       if ticket.reviewed?
-        review = ticket.review
         link_to(company_ticket_review_path(ticket), class: 'review-link') do
-          render('company/shared/reviewable_content', review: review)
+          render('company/shared/reviewable_content', review: ticket.review)
         end
       else
         content_tag(:span, 'Not Rated',
