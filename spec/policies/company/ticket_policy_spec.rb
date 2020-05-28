d5e24c9c0e30616435b3543661fee0457427cdc1
Vanya <vanyavasylyshyn@gmail.com>
2020-05-28 16:25:10 +0300

    added policy tests.

--- spec/policies/company/ticket_policy_spec.rb
+++ spec/policies/company/ticket_policy_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Company::TicketPolicy, type: :policy do
 
   subject { described_class }
 
+  permissions :show? do
+    it 'grant access' do
+      expect(subject).to permit(unit.responsible_user, unit.tickets.last)
+      expect(subject).to permit(unit.tickets.last.user, unit.tickets.last)
+    end
+
+    it 'denied access' do
+      expect(subject).not_to permit(company_owner, unit.tickets.last)
+      expect(subject).not_to permit(employee, unit.tickets.last)
+    end
+  end
+
   permissions :create? do
     it 'grant access' do
       expect(subject).to permit(company_owner)
@@ -14,6 +26,17 @@ RSpec.describe Company::TicketPolicy, type: :policy do
 
     it 'denied access' do
       expect(subject).not_to permit(staff_member)
+    end
+  end
+
+  permissions :update? do
+    it 'grant access' do
+      expect(subject).to permit(unit.tickets.last.user, unit.tickets.last)
+    end
+
+    it 'denied access' do
+      expect(subject).not_to permit(company_owner, unit.tickets.last)
+      expect(subject).not_to permit(employee, unit.tickets.last)
     end
   end
 
