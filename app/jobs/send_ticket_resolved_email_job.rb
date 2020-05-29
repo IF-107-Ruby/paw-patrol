b1f0f991b453286c8848d4fb8393f519a62645b0
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-29 23:11:37 +0300

    Fix remarks

--- app/jobs/send_ticket_resolved_email_job.rb
+++ app/jobs/send_ticket_resolved_email_job.rb
@@ -2,7 +2,7 @@ class SendTicketResolvedEmailJob < ApplicationJob
   queue_as :default
 
   def perform(ticket_id)
-    ticket = Ticket.find(ticket_id)
+    ticket = Ticket.find_by(id: ticket_id)
     return if ticket.nil?
 
     TicketMailer.ticket_resolved_email(ticket).deliver_now
--- spec/jobs/send_ticket_resolved_message_job_spec.rb
+++ spec/jobs/send_ticket_resolved_message_job_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SendTicketResolvedEmailJob, type: :job do
+  include_context 'employee with ticket'
+
   describe '#perform' do
     it 'calls TicketMailer' do
-      ticket = double('ticket', id: 1)
-      allow(Ticket).to receive(:find).and_return(ticket)
       allow(TicketMailer).to receive_message_chain(:ticket_resolved_email, :deliver_now)
 
-      described_class.new.perform(ticket_id: ticket.id)
+      described_class.new.perform(ticket.id)
 
       expect(TicketMailer).to have_received(:ticket_resolved_email).with(ticket)
     end
@@ -15,7 +15,7 @@ RSpec.describe SendTicketResolvedEmailJob, type: :job do
 
   describe '#perform later' do
     it 'adds the job to the queue' do
-      described_class.perform_later(1)
+      described_class.perform_later(ticket.id)
 
       expect(enqueued_jobs.last[:job]).to eq described_class
     end
