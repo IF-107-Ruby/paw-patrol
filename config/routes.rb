595da4feb14118f42e3897f14cacc2bb6c04d12a
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-05-29 12:08:15 +0300

    Add association with ticket commentators

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  authenticate :user, lambda { |user| user.admin? } do
+  authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
 
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Ticket, type: :model do
     it { expect(ticket).to belong_to(:user) }
     it { expect(ticket).to belong_to(:unit) }
     it { expect(ticket).to have_one(:review).dependent(:destroy) }
+    it { is_expected.to have_many(:commentators).through(:comments) }
   end
 
   describe 'validations' do
