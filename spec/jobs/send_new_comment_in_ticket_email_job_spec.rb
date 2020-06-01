99b70d766f86c4eb90a42cc068ed96c413752403
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-06-01 20:19:19 +0300

    Add SendNewCommentInTicketEmailJob

--- spec/jobs/send_new_comment_in_ticket_email_job_spec.rb
+++ spec/jobs/send_new_comment_in_ticket_email_job_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe SendNewCommentInTicketEmailJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include_context 'employee with ticket'
+
+  describe '#perform' do
+    before do
+      allow(TicketMailer).to receive_message_chain(:ticket_has_comment_email, :deliver_now)
+    end
+
+    it 'calls TicketMailer' do
+      described_class.new.perform(ticket.id)
+
+      expect(TicketMailer).to have_received(:ticket_has_comment_email).with(ticket)
+    end
+
+    it 'not calls TicketMailer it ticket id not exist' do
+      described_class.new.perform(ticket.id + 100)
+
+      expect(TicketMailer).not_to have_received(:ticket_has_comment_email).with(ticket)
+    end
+  end
+
+  describe '#perform later' do
+    it 'adds the job to the queue' do
+      described_class.perform_later(ticket.id)
+
+      expect(enqueued_jobs.last[:job]).to eq described_class
+    end
+  end
 end
