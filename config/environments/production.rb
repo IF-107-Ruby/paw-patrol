aea7a351c3cac4a822d6373d776a7bc70c9563b3
Liubomyr <lyubomur.user.mv@gmail.com>
2020-06-02 18:30:33 +0300

    Change delivery_method to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,8 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { host: ENV['RAILS_HOST'] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost',
-                                         host: 'roompassport.pp.ua',
-                                         port: 25 }
+  config.action_mailer.delivery_method = :sendmail
 end
