4db6bcd0d439f4b4e5193b6af6aaea89fe031796
Liubomyr <lyubomur.user.mv@gmail.com>
2020-06-02 15:46:45 +0300

    Add mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['RAILS_HOST'] }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost',
+                                         host: 'roompassport.pp.ua',
+                                         port: 25 }
 end
