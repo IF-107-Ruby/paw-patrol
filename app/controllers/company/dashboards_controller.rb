f9f9ced2cf48663388a625bf6ab2eb1891cb3e25
Vanya <vanyavasylyshyn@gmail.com>
2020-06-04 12:15:57 +0300

    created review satisfaction chart.

--- app/controllers/company/dashboards_controller.rb
+++ app/controllers/company/dashboards_controller.rb
@@ -1,4 +1,31 @@
 class Company
   class DashboardsController < Company::BaseController
+    before_action :read_satisfaction_data, only: %i[satisfaction]
+
+    def satisfaction
+      if @satisfaction.any?
+        render json: @satisfaction
+      else
+        flash.now[:warning] = 'Satisfaction is empty!'
+        render :show
+      end
+    end
+
+    private
+
+    def read_satisfaction_data
+      @satisfaction = []
+      (1..5).each do |rating|
+        @satisfaction.push(get_amount_of_satisfaction_by_rating(rating))
+      end
+    end
+
+    def get_amount_of_satisfaction_by_rating(rating)
+      ticket_ids = current_company.tickets.split(',')
+      {
+        name: rating,
+        amount: Review.where(rating: rating, ticket: ticket_ids).count
+      }
+    end
   end
 end
--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -4,10 +4,12 @@ import WatchersBlock from "../bundles/app/ticketShowPage/components/WatchersBloc
 import Calendar from "../bundles/app/Units/components/Calendar";
 import Units from "../bundles/app/shared/components/Units";
 import QrCode from "qrcode.react";
+import RewiewSatisfaction from "../bundles/app/RewiewSatisfaction/components/RewiewSatisfaction";
 
 ReactOnRails.register({
   WatchersBlock,
   Calendar,
   QrCode,
   Units,
+  RewiewSatisfaction,
 });
