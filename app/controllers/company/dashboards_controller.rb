78e8a028a38249a238604292b23ebae54c83b12f
Vanya <vanyavasylyshyn@gmail.com>
2020-06-09 18:39:12 +0300

    moved large business logic from controller to service object.

--- app/controllers/company/dashboards_controller.rb
+++ app/controllers/company/dashboards_controller.rb
@@ -1,6 +1,5 @@
 class Company
   class DashboardsController < Company::BaseController
-    before_action :read_satisfaction_data, only: %i[satisfaction]
 
     def show
       if current_user.staff_member? || current_user.employee?
@@ -13,12 +12,8 @@ class Company
     end
 
     def satisfaction
-      if @satisfaction.any?
-        render json: @satisfaction
-      else
-        flash.now[:warning] = 'Satisfaction is empty!'
-        render :show
-      end
+      @satisfaction = ReadSatisfaction.call(current_company)
+      render json: @satisfaction
     end
 
     private
@@ -32,21 +27,6 @@ class Company
                                       .most_recent
                                       .limit(7)
                                       .decorate
-    end
-
-    def read_satisfaction_data
-      @satisfaction = []
-      (1..5).each do |rating|
-        @satisfaction.push(get_amount_of_satisfaction_by_rating(rating))
-      end
-    end
-
-    def get_amount_of_satisfaction_by_rating(rating)
-      ticket_ids = current_company.tickets.split(',')
-      {
-        name: "Rated #{rating}",
-        amount: Review.where(rating: rating, ticket: ticket_ids).count
-      }
     end
   end
 end
