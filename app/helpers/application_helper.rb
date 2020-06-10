5921be12e3349e2302399d86ab68532b19b7755a
Liubomyr <lyubomur.user.mv@gmail.com>
2020-06-10 11:36:11 +0300

    Move footers bottom text to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
   def active_path(path)
     'active' if current_page?(path)
   end
+
+  def footer_bottom_text
+    "Copyright &copy; <b>Room Passport.SoftServe</b> #{Time.zone.now.strftime('%Y')}"
+  end
+
+  def small_footer_bottom_text
+    "&copy; #{Time.zone.now.strftime('%Y')}" \
+      ' <b>Room Passport.SoftServe</b>. All Rights Reserved.'
+  end
 end
