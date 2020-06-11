223cb40ea489850a1cd753b1aee651868ecb0251
Vanya <vanyavasylyshyn@gmail.com>
2020-06-11 14:36:25 +0300

    added confirmable to schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_531_160_131) do
+ActiveRecord::Schema.define(version: 20_200_611_104_502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -148,6 +148,12 @@ ActiveRecord::Schema.define(version: 20_200_531_160_131) do
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
     t.integer 'role', default: 0, null: false
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.index ['confirmation_token'],
+            name: 'index_users_on_confirmation_token',
+            unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'],
             name: 'index_users_on_reset_password_token',
@@ -199,6 +205,4 @@ ActiveRecord::Schema.define(version: 20_200_531_160_131) do
   add_foreign_key 'users_companies_relationships', 'users'
   add_foreign_key 'users_units_relationships', 'units'
   add_foreign_key 'users_units_relationships', 'users'
-  add_foreign_key 'watchers_relationships', 'tickets'
-  add_foreign_key 'watchers_relationships', 'users'
 end
