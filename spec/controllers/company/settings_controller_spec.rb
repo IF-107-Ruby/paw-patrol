ae956d94aa3a34b83c4664c5c5f688581407e791
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-06-13 22:28:28 +0300

    Refactor SettingsController №2

--- spec/controllers/company/settings_controller_spec.rb
+++ spec/controllers/company/settings_controller_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe Company::SettingsController, type: :controller do
   render_views
 
+  let(:user_password) { '123456' }
   let!(:company) { create(:company) }
-  let!(:employee) { create(:employee, company: company) }
+  let!(:employee) do
+    create(:employee,
+           password: user_password,
+           password_confirmation: user_password,
+           company: company)
+  end
 
   before do
     sign_in employee
@@ -30,6 +36,49 @@ RSpec.describe Company::SettingsController, type: :controller do
     context 'With invalid params' do
       before do
         put :update, params: { user_settings: { first_name: '', last_name: '' } }
+      end
+
+      it { is_expected.not_to set_flash[:success] }
+      it { is_expected.to set_flash.now[:warning] }
+      it { is_expected.to render_template('edit') }
+    end
+  end
+
+  describe 'Patch#Update_password' do
+    context 'With valid params' do
+      before do
+        put :update_password, params: {
+          user_password_settings: { current_password: user_password,
+                                    password: user_password.reverse,
+                                    password_confirmation: user_password.reverse }
+        }
+      end
+
+      it { is_expected.to set_flash[:success] }
+      it { is_expected.to redirect_to(company_settings_path) }
+    end
+
+    context 'With incorrect current password' do
+      before do
+        put :update_password, params: {
+          user_password_settings: { current_password: 'asdasdasd',
+                                    password: user_password.reverse,
+                                    password_confirmation: user_password.reverse }
+        }
+      end
+
+      it { is_expected.not_to set_flash[:success] }
+      it { is_expected.to set_flash.now[:warning] }
+      it { is_expected.to render_template('edit') }
+    end
+
+    context 'With invalid params' do
+      before do
+        put :update_password, params: {
+          user_password_settings: { current_password: user_password,
+                                    password: '1',
+                                    password_confirmation: '2' }
+        }
       end
 
       it { is_expected.not_to set_flash[:success] }
--- spec/features/company/user_changes_settings_spec.rb
+++ spec/features/company/user_changes_settings_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 feature 'User changes settings' do
+  let!(:user_password) { '123456' }
   let!(:company) { create(:company) }
-  let!(:employee) { create(:employee, company: company) }
+  let!(:employee) do
+    create(:employee,
+           password: user_password,
+           password_confirmation: user_password,
+           company: company)
+  end
   let(:user_valid_params) { FactoryBot.attributes_for(:user) }
   let(:user_invalid_params) do
     { first_name: '', last_name: '', email: '', password: '' }
@@ -22,10 +28,10 @@ feature 'User changes settings' do
     click_on 'Save Changes'
 
     expect(page).to have_selector('.notification.success.closeable',
-                                  text: 'Settings updated!')
+                                  text: 'Account settings updated!')
   end
 
-  scenario 'successfully changes profile settings' do
+  scenario 'unsuccessfully changes profile settings' do
     fill_in 'user_settings_first_name', with: user_invalid_params[:first_name]
     fill_in 'user_settings_last_name', with: user_invalid_params[:last_name]
     fill_in 'user_settings_email', with: user_invalid_params[:email]
@@ -33,32 +39,34 @@ feature 'User changes settings' do
     click_on 'Save Changes'
 
     expect(page).to have_css('.notification.warning.closeable',
-                             text: 'Settings is not updated!')
+                             text: 'Account settings is not updated!')
     expect(page).to have_css('#error_explanation')
   end
 
   scenario 'successfully changes password' do
     expect(page).to have_css('h3', text: 'Password & Security')
 
-    fill_in 'user_settings_password', with: user_valid_params[:password]
-    fill_in 'user_settings_password_confirmation',
+    fill_in 'user_password_settings_current_password', with: user_password
+    fill_in 'user_password_settings_password', with: user_valid_params[:password]
+    fill_in 'user_password_settings_password_confirmation',
             with: user_valid_params[:password_confirmation]
 
     click_on 'Change password'
 
     expect(page).to have_selector('.notification.success.closeable',
-                                  text: 'Settings updated!')
+                                  text: 'Password settings updated!')
   end
 
   scenario 'unsuccessfully changes password' do
-    fill_in 'user_settings_password', with: user_invalid_params[:password]
-    fill_in 'user_settings_password_confirmation',
-            with: user_invalid_params[:password]
+    fill_in 'user_password_settings_current_password', with: 'asdasdsad'
+    fill_in 'user_password_settings_password', with: user_invalid_params[:password]
+    fill_in 'user_password_settings_password_confirmation',
+            with: user_invalid_params[:password_confirmation]
 
     click_on 'Change password'
 
     expect(page).to have_css('.notification.warning.closeable',
-                             text: 'Settings is not updated!')
+                             text: 'Password settings is not updated!')
     expect(page).to have_css('#error_explanation')
   end
 end
