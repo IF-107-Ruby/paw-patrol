0859b3a01c9008f3497bbcad18f60918f24d7c41
Andrii Hurskyi <andriy_gurskyy@ukr.net>
2020-06-17 10:54:22 +0300

    Add list of registered companies

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,6 +2,7 @@ module Admin
   class DashboardsController < Admin::BaseController
     def index
       authorize(%i[admin dashboard])
+      @pagy, @companies = pagy_decorated(Company.all, items: 10)
     end
   end
 end
