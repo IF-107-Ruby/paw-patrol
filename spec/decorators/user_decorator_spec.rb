0d407a2c25a01b58691a0a6c8384f608fce1250f
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-06-17 11:16:52 +0300

    Add created_tickets_count and resolved_tickets_count methods at User decorator

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
-  let(:user) { create(:user) }
-  let(:decorated_user) { user.decorate }
+  include_context 'employee with ticket'
+
+  let(:decorated_employee) { employee.decorate }
+  let(:responsible_user) { unit.responsible_user }
+  let(:responsible_user_decorated) { responsible_user.decorate }
 
   it 'full user name' do
-    expect(decorated_user.full_name).to eq(user.first_name + ' ' + user.last_name)
+    expected = employee.first_name + ' ' + employee.last_name
+    expect(decorated_employee.full_name).to eq(expected)
+  end
+
+  it 'created tickets count' do
+    tickets_count = employee.tickets.count
+    expected = "#{tickets_count} ticket".pluralize(tickets_count)
+    expect(decorated_employee.created_tickets_count).to eq(expected)
+  end
+
+  it 'resolved tickets count' do
+    tickets_count = responsible_user.resolved_tickets.count
+    expected = "#{tickets_count} ticket".pluralize(tickets_count)
+    expect(responsible_user_decorated.resolved_tickets_count).to eq(expected)
   end
 end
