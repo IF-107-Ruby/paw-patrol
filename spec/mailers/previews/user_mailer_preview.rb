9cabffb1fe26b619ba18f6dba5894212dec6e7e8
Vanya Vasylyshyn <46623271+vanyavasylyshyn@users.noreply.github.com>
2020-06-18 10:06:20 +0300

    Fix/email styling (#145)
    * changed style set for emails.
    
    * changed email style layout.

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def invitation_email
-    UserMailer.invitation_email(User.last)
+    UserMailer.invitation_email(User.last, 'password')
   end
 end
